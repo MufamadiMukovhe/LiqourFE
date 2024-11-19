57b621e247af46b0999b32ba30b1b13c5df0fbf9
Phumudzo98 <phumu98@gmail.com>
2024-11-19 07:32:35 +0200

    env

--- src/app/pages/my-tasks/my-tasks.page.ts
+++ src/app/pages/my-tasks/my-tasks.page.ts
@@ -52,16 +52,11 @@ export class MyTasksPage implements OnInit {
 
         if(this.role==='INSPECTOR')
           {
-<<<<<<< HEAD
-            console.log(this.collect)
-            this.collect = response.filter(item => item.action === 'Complete Report' || item.action === 'Complete Inspection Report' || item.action === 'Inspector Serve Summons' || item.action === 'Inspector Serve Section 22(5) Notice' || item.action === 'Complete Supplementary Report');
-=======
             //this.collect = response;
             this.collect = response.filter(item => item.action === 'Complete Report' || item.action === 'Complete Inspection Report' || item.action === 'Inspector Serve Summons' || item.action === 'Inspector Serve Section 22(5) Notice' || item.action === 'Complete Supplementary Report'|| item.action === 'Complete Report Query');
 
 
           
->>>>>>> a4a763a91902783421d8ce121ad63cc52b27cf0d
             this.collect = this.collect.filter(item => item.status!=='Complete')
 
             console.log(this.collect);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,16 +1,8 @@
 export const environment = {
   production: true,
    googleMapsApiKey:'AIzaSyCs2lLjDJYbkPST_4NoPOY7K_tA2jd0siE',
-<<<<<<< HEAD
-   
-=======
-   //eclbDomain: "https://system.eclb.co.za/eclb2/",
-   //eclbDomain:"http://localhost:8081/"
-   //eclbDomain: "https://system.eclb.co.za/eclb2/",
-   //eclbDomain:"http://localhost:8081/"
-   //eclbDomain:"http://localhost:8081/"
->>>>>>> a4a763a91902783421d8ce121ad63cc52b27cf0d
-    eclbDomain:"http://localhost:8081/"
 
-    //eclbDomain: "https://system.eclb.co.za/eclb2/"
+    //eclbDomain:"http://localhost:8081/"
+
+    eclbDomain: "https://system.eclb.co.za/eclb2/"
 };
